c46aeb8685f85138948508ea165ae582625e9c1d
Tomás Gray <tomas.a.gray@gmail.com>
2022-08-02 23:12:21 -0700

    Update helper batch scripts

--- export.cmd
+++ export.cmd
@@ -2,12 +2,12 @@
 @echo off
 
 :: Zip path
-Set zip-path=C:\Program Files\7-Zip
+Set "zip-path=C:\Program Files\7-Zip"
 Set "zip=%zip-path%\7z.exe"
 
 :: Kodi addon directory
 Set "export-dir=%1"
-Set "work-dir=%USERPROFILE%\Projects\Source\PyCharmProjects\plugin.matchday"
+Set "work-dir=%~dp0"
 :: Top-level files & dirs
 Set "main=%work-dir%\main.py"
 Set "addon=%work-dir%\addon.xml"
@@ -21,14 +21,15 @@ echo Creating temporary directory...
 IF EXIST %archive% (rmdir /S /Q %archive%)
 mkdir %archive%
 :: Copy files to tmp dir
-echo Copying files to tmp dir
+echo Copying files to tmp dir: %archive%...
 COPY %main% "%archive%\"
 COPY %addon% "%archive%\"
 COPY %lic% "%archive%\"
-XCOPY /e /y %resources% "%archive%\resources\"
+XCOPY /e /y /q %resources% "%archive%\resources\"
 echo Zipping plugin...
 "%zip%" a -tzip "%archive%.zip" "%archive%"
 :: Copy archive to export dir
+echo Exporting plugin to: %export-dir%...
 MOVE "%archive%.zip" %export-dir%
 :: Remove temp files
 echo Removing temporary files...
--- install.cmd
+++ install.cmd
@@ -1,17 +1,19 @@
 :: Installs (updates) plugin files in Kodi installation directory.
-@ECHO off
+@echo off
 
+echo Installing Matchday Plugin for Kodi...
 :: Kodi addon directory
 Set "kodi-dir=%APPDATA%\Kodi\addons\plugin.matchday\"
-Set "work-dir=%USERPROFILE%\Projects\Source\PyCharmProjects\plugin.matchday"
+Set "work-dir=%~dp0"
 :: Top-level files & dirs
 Set "main=%work-dir%\main.py"
 Set "addon=%work-dir%\addon.xml"
 Set "resources=%work-dir%\resources"
 
 echo Copying data from %main% to %kodi-dir%...
+echo Installing addon...
 COPY %main% %kodi-dir%
 COPY %addon% %kodi-dir%
-XCOPY /y /s %resources% "%kodi-dir%resources\"
-echo.
+echo Installing resources...
+XCOPY /y /s /q %resources% "%kodi-dir%resources\"
 echo Done.
